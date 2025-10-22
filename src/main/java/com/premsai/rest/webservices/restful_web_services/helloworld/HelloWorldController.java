3fd6b8ce8eef18983ef9ba5803c2cff7936e5f43
premsai-20 <sakamurisai252@gmail.com>
2025-10-22 05:51:30 +0530

    - RestAPI Documentation
    - Content Negotiation
    
    - Internationalization
    
    - Versioning

--- src/main/java/com/premsai/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/premsai/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.premsai.rest.webservices.restful_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource){
+		this.messageSource=messageSource;
+	}
 	@GetMapping(path= "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
@@ -21,4 +30,11 @@ public class HelloWorldController {
 	public  HelloWorldBean helloWorldPathVariable(@PathVariable String name, @PathVariable int id) {
 		return new HelloWorldBean(String.format("Hello World, %s, %d",name, id));
 	}
+	
+	@GetMapping(path= "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+		//return "Hello World v2";
+	} 
 }
--- src/main/java/com/premsai/rest/webservices/restful_web_services/user/User.java
+++ src/main/java/com/premsai/rest/webservices/restful_web_services/user/User.java
@@ -2,10 +2,17 @@ package com.premsai.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
+	
+	@Size(min=2, message="Name should have atleast 2 letters")
 	private String name;
+	
+	@Past(message="Birthday should be in past tense")
 	private LocalDate birthDate;
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
